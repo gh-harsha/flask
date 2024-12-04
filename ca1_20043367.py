1ed3b7ca2e693d3c50fcbaa7dc8c1757a046638a
gh-harsha <147692477+gh-harsha@users.noreply.github.com>
2024-12-04 11:54:46 +0000

    Comment on line 357

--- ca1_20043367.py
+++ ca1_20043367.py
@@ -354,7 +354,7 @@ rows
 from flask import Flask
 from flask import render_template
 from flask import request
-!pip install flask_cors
+#!pip install flask_cors
 from flask_cors import CORS
 
 app = Flask(__name__)
